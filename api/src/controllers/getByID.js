014a5dc1c3791c4be9abbbbe6ce84506b8ecfdc2
Gian <gianpolastri@gmial.com>
2023-05-07 14:01:01 -0300

    getByID terminado

--- api/src/controllers/getByID.js
+++ api/src/controllers/getByID.js
@@ -1,26 +1,26 @@
 const axios = require("axios");
 require("dotenv").config();
 const { API_URL } = process.env;
-const pokemonCleaner = require('../utils/pokemonCleaner');
+const pokemonCleaner = require("../utils/pokemonCleaner");
+const {Pokemon} = require('../db');
 
 const getByID = async (id, source) => {
-  
-    const rawPokemon = []
-    const pokemon =
+  const rawPokemon = [];
+  const pokemon =
     source === "db"
-      ? console.log("NIY")
+      ? await Pokemon.findbyPk(id)
       : await axios
           .get(`${API_URL}/pokemon/${id}`)
           .then((response) => response.data);
 
-    rawPokemon.push(pokemon);
-    
-    if(source === 'db'){
-        return rawPokemon[0];
-    }else{
-        const cleanPokemon = pokemonCleaner(rawPokemon);
-        return cleanPokemon[0];
-    }
+  rawPokemon.push(pokemon);
+
+  if (source === "db") {
+    return rawPokemon[0];
+  } else {
+    const cleanPokemon = pokemonCleaner(rawPokemon);
+    return cleanPokemon[0];
+  }
 };
 
 module.exports = getByID;
