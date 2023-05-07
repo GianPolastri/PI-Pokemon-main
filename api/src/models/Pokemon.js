66ed7eb1908390ea382629b3ebf227816c9ec455
Gian <gianpolastri@gmial.com>
2023-05-07 11:24:26 -0300

    1er version modelos y relaciones

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id:{
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
+    hp:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    atk:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    def:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    speed:{
+      type: DataTypes.INTEGER,
+    },
+    height:{
+      type: DataTypes.INTEGER,
+    },
+    weight:{
+      type: DataTypes.INTEGER,
+    },
+    created:{
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    }
   }, {timestamps: false});
 };
--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -2,6 +2,16 @@ const {DataTypes} = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('type', {
-
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        }
     }, {timestamps: false})
 }
