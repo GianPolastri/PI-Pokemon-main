54c7ed5ef66522955f37ecbac13cac276af1a2f4
Gian <gianpolastri@gmial.com>
2023-05-15 18:28:36 -0300

    primer diseño de paginado

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Cards from '../../Components/Cards/Cards'
 import {useDispatch, useSelector} from 'react-redux'
@@ -6,14 +6,28 @@ import { getAllPokemons } from '../../Redux/Actions/Actions'
 const Home = () => {
   const dispatch = useDispatch();
   const allPokemons = useSelector(state=>state.allPokemons);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(()=>{
     dispatch(getAllPokemons())
   },[])
 
+  const pokemonPartition = () => {
+
+    return allPokemons.slice(currentPage,currentPage+12);
+  }
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 12)
+  }
+
+  const prevPage = () => {
+    if(currentPage>0) setCurrentPage(currentPage - 12)
+  }
+
   return (
     <div>
-      <Cards allPokemons={allPokemons}/>  
+      <Cards pokemonPartition={pokemonPartition} nextPage={nextPage} prevPage={prevPage}/>  
     </div>
   )
 }
