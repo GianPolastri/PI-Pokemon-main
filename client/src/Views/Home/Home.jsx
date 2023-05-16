da6d2dc6319094aa3087d6c41e46fecea160399c
Gian <gianpolastri@gmial.com>
2023-05-16 15:50:09 -0300

    SearchSubmit ahora busca por ID, UUID y name

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -39,11 +39,24 @@ const Home = () => {
   }
 
   const searchSubmit = async () => {
+    const regex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+    console.log(typeof(search));
     try {
-      const pokemonByName = ((await axios.get(`http://localhost:3001/pokemon/name?name=${search}`)).data);
-      console.log(pokemonByName);
-      setPokemonFinder({pokemonFound: pokemonByName});
-      console.log(pokemonFinder);
+      if(regex.test(search)){
+        console.log('entre aca');
+        const pokemonByUUID = await (axios.get(`http://localhost:3001/pokemon/${search}`)).then(response => response.data);
+        console.log(pokemonByUUID);
+        setPokemonFinder({pokemonFound: pokemonByUUID});
+      }else if(!isNaN(search)){
+        const pokemonByID = (await axios.get(`http://localhost:3001/pokemon/${search}`)).data
+        setPokemonFinder({pokemonFound: [pokemonByID]})
+
+      }else{
+        const pokemonByName = ((await axios.get(`http://localhost:3001/pokemon/name?name=${search}`)).data);
+        // console.log(pokemonByName);
+        setPokemonFinder({pokemonFound: pokemonByName});
+        // console.log(pokemonFinder);
+      }
     } catch (error) {
       alert(error.message)
     }
