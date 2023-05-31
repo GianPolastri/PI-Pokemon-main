b6565571594506276cd7127623f05553bef4dae9
Gian <gianpolastri@gmial.com>
2023-05-31 16:02:04 -0300

    Corrección hecha en el review del PI

--- api/src/handlers/getAllTypes.js
+++ api/src/handlers/getAllTypes.js
@@ -6,7 +6,12 @@ const getTypesFromDB = require('../controllers/getTypesFromDB')
 module.exports = async (req, res) => {
     try {
         const response = await getTypesFromDB();
-        res.status(200).json(response);
+        if(response.length){
+          res.status(200).json(response);
+        }else{
+          const typesFormAPI = getTypesFromAPI();
+          res.status(200).json(typesFormAPI);
+        }
     } catch (error) {
       res.status(400).json({error: error.message});
     }
